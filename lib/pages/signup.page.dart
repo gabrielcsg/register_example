e03a44a04b7ad297ab5618747de7c433d1b2c50e
Gabriel <gabrielcssg@gmail.com>
2019-09-22 01:43:56 -0300

    sign up

---
+++ lib/pages/signup.page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 30, left: 40, right: 40),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            Container(
+              width: 200,
+              height: 200,
+              alignment: Alignment(0.0, 0.0),
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: AssetImage('assets/user.png'),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Icon(
+                    IconData(
+                      57669,
+                      fontFamily: 'MaterialIcons',
+                    ),
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(
+                fontSize: 28,
+              ),
+            ),
+            SizedBox(height: 40,),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: FlatButton(
+                child: Text(
+                  "Cadastrar-se",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 40,
+              alignment: Alignment.center,
+              child: FlatButton(
+                child: Text(
+                  'Cancelar',
+                  textAlign: TextAlign.center,
+                ),
+                onPressed: () {}, // => Navigation.pop(context, false),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
